f9b20c853184f154331df29295f95e5794b67d2d
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com>
2024-08-30 19:09:57 +0100

    add extra lib

--- iconHelper.js
+++ iconHelper.js
@@ -1,7 +1,7 @@
 const iconKeys = [
   'fa', 'md', 'io', 'ti', 'go', 'fi', 'gi', 'wi', 'ai', 'bs', 'bi', 'ci', 'cg', 
   'di', 'fa6', 'gr', 'hi', 'hi2', 'lia', 'io5', 'lu', 'pi', 'im', 'rx', 'ri', 
-  'si', 'sl', 'tb', 'tfi', 'vsc'
+  'si', 'sl', 'tb', 'tfi', 'vsc', 'fc'
 ];
 
 const iconPackages = iconKeys.reduce((acc, key) => {
